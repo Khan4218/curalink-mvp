ce15137db36515b0a38138cba3d94ccf5916c862
Akbar Khan <mirakber4@gmail.com>
2025-11-01 01:49:31 +0530

    create patient and researcher onbarding pages

---
+++ curalink-mvp/app/patient/onboarding/page.tsx
@@ -0,0 +1,7 @@
+export default function PatientOnboarding() {
+  return (
+    <div className="p-6 text-xl">
+      Patient Onboarding Page
+    </div>
+  );
+}
---
+++ curalink-mvp/app/researcher/onboarding/page.tsx
@@ -0,0 +1,7 @@
+export default function ResearcherOnboarding() {
+  return (
+    <div className="p-6 text-xl">
+      Researcher Onboarding Page
+    </div>
+  );
+}
